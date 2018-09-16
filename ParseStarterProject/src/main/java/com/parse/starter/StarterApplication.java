8b981a6ba4ed8040acc8313608c1d10295ed6010
Varis Darasirikul <varis.dara@gmail.com>
2018-09-17 00:41:01 +0800

    Init project

---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("23057e804c4464e8389e8f7c4de0df4eb12a5f33")
+            .clientKey("3e8f4e98b19a03d3b1804e22e5e183b0ea209a89")
+            .server("http://54.213.67.110:80/parse/")
+            .build()
+    );
+
+//    ParseObject object = new ParseObject("ExampleObject");
+//    object.put("myNumber", "123");
+//    object.put("myString", "rob");
+//
+//    object.saveInBackground(new SaveCallback () {
+//      @Override
+//      public void done(ParseException ex) {
+//        if (ex == null) {
+//          Log.i("Parse Result", "Successful!");
+//        } else {
+//          Log.i("Parse Result", "Failed" + ex.toString());
+//        }
+//      }
+//    });
+
+
+    // ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
